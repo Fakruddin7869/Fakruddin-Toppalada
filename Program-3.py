1206a0c4f47fd17bdde56561e9353d64af44d87c
Fakruddin sheikh <136787727+Fakruddin7869@users.noreply.github.com>
2025-11-19 10:49:40 +0530

    Program-3.py
    This program prints a sequence of odd numbers starting from 1, based on a single integer input a.
    
    The user enters an integer value a.
    The program computes the first a odd numbers using the formula:odd number = 2*i − 1
    It stores all generated odd numbers in a list.
    Finally, it prints the list in comma-separated format.

--- Program-3.py
+++ Program-3.py
@@ -1,1 +1,11 @@
+a = int(input("Enter a number: "))
 
+# If even, reduce by 1 (so we print only odd numbers)
+if a % 2 == 0:
+    a = a - 1
+
+series = []
+for i in range(1, a + 1, 2):
+    series.append(i)
+
+print(*series, sep=", ")
